a55ce6f67e13d9b101cecf96fbd2085dd751176c
Camila Cano <camilacanog@gmail.com>
2013-10-31 12:36:51 -0400

    presentation

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+var map;
+var markers = [];
+
+$(function(){
+	map = '<img src="http://maps.googleapis.com/maps/api/staticmap?center=10010&zoom=13&size=1000x1000&sensor=false">';
+	$('#map_div').append(map);
+	
+
+});
